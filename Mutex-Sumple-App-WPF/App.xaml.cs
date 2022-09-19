c7a5a4dfc38408a1a81b5aeda0e2267c2cea0048
shimnamisan <mikan.sup.all@gmail.com>
2022-09-19 09:50:41 +0900

    プロジェクト ファイルを追加します。

---
+++ Mutex-Sumple-App-WPF/App.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace Mutex_Sumple_App_WPF
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    // Mutex名（アプリケーション固有の名前. 他のアプリケーションと被らない用に注意）
+    private const string _mutexName = "MutexTest";
+
+    // Mutexオブジェクトを作成
+    private Mutex _mutex = new Mutex(false, _mutexName);
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public App()
+    {
+
+      // 起動時のイベントにメソッドを登録
+      Startup += Application_Startup;
+
+      // アプリケーション終了時のイベントにメソッドを登録
+      Exit += Application_Exit;
+    }
+
+    /// <summary>
+    /// スタートアップ時のイベントに読み込ませるメソッド
+    /// </summary>
+    /// <param name="sender">イベントを送信したオブジェクト</param>
+    /// <param name="e">イベント引数</param>
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+
+      // ミューテックスの所有権を要求
+      if (!_mutex.WaitOne(0, false))
+      {
+        // 既に起動しているため終了させる
+        MessageBox.Show("ソフトウェアは既に起動しています。",
+                        "二重起動防止",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Exclamation);
+
+        // インスタンスが所有しているリソースを開放する
+        _mutex.Close();
+
+        // null を代入
+        _mutex = null;
+
+        // アプリケーションを終了させる
+        Environment.Exit(0);
+      }
+    }
+
+    /// <summary>
+    /// アプリケーション終了時のイベントに登録するメソッド
+    /// </summary>
+    /// <param name="sender">イベントを送信したオブジェクト</param>
+    /// <param name="e">イベント引数</param>
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+
+      if (_mutex != null)
+      {
+        // 共有リソースである mutex を開放
+        _mutex.ReleaseMutex();
+
+        // インスタンスが所有しているリソースを開放する
+        _mutex.Close();
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Mutex-Sumple-App-WPF
+
+WPF で Mutex を使用した多重起動を禁止する動作の実装を確認しました。
+
+# Development
+
+- Visual Studio 2022
+- .NET6
+
+# Features
+
+- アプリケーションの多重起動を禁止
+
+# DEMO
+
+![dual-boot-injunction](https://user-images.githubusercontent.com/49751604/190935370-a15c8f56-13e9-4562-ada6-2ce5b18d00d2.gif)
+
+# Author
+
+H.Nishihara
+
+Twitter: [@hn_pgtech](https://twitter.com/hn_pgtech)
+
+Blog: https://blog.hn-pgtech.com/
+
+# License
+
+Mutex-Sumple-App-WPF is under [MIT license](https://en.wikipedia.org/wiki/MIT_License).
